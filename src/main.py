3c99a1667b813f3ceb42d9976112e64aa4e19d8a
benschaser <benschaser@gmail.com>
2025-08-27 16:29:49 -0500

    load unsolved

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 from board import Board
+from solver import Solver
 
 def main():
     test_file = "../test/s1.txt"
     board = Board.load_board(test_file)
-    board.print_cross_lite(3,5)
+    # board.print_cross_lite(3,5)
+
+    solver = Solver
+    solver.load_unsolved(board)
+
 
 
 if __name__ == "__main__":
--- src/solver.py
+++ src/solver.py
@@ -1,10 +1,20 @@
+from board import Board
+
 def clear_terminal():
     print("\033[2J\033[H", end="")
 
 
 class Solver:
     def __init__(self):
-        pass
+        self.unsolved = {}
+
+    def load_unsolved(self): 
+        for r in range(0, 9, 1):
+            for c in range(0, 9, 1):
+                if (self.board.get_cell(c, r) == 0):
+                    self.unsolved[(c, r)] = []
 
     def solve(self, board):
-        pass
+        self.board = board
+        self.load_unsolved()
+        print(self.unsolved)
