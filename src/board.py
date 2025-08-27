19090abb19d54f8e31ef232a4d47d2eee7f00b83
benschaser <benschaser@gmail.com>
2025-08-27 16:07:37 -0500

    updated highlighting

--- src/board.py
+++ src/board.py
@@ -1,6 +1,10 @@
 RESET = "\033[0m"
 LITE = "\033[7m"
-HL = "---------+-----------+---------"
+HL = "├──────────┼───────────┼──────────┤"
+TL = "┌──────────┬───────────┬──────────┐"
+BL = "└──────────┴───────────┴──────────┘"
+def cell_f(num):
+        return num if num != 0 else "□"
 
 class Board:
     def __init__(self, grid):
@@ -31,36 +35,75 @@ class Board:
     def set_cell(self, x, y, val):
         self.grid[y][x] = val
 
+    
+    
     def print_row(self, row_index, full_lite, col_lite):
         r = self.grid[row_index]
         if (full_lite):
-            print(LITE + "{}  {}  {}  |  {}  {}  {}  |  {}  {}  {}".format(r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[7], r[8]) + RESET)
+            print("│" + LITE + " {}  {}  {}  |  {}  {}  {}  |  {}  {}  {} ".format(
+                cell_f(r[0]), 
+                cell_f(r[1]), 
+                cell_f(r[2]), 
+                cell_f(r[3]), 
+                cell_f(r[4]), 
+                cell_f(r[5]), 
+                cell_f(r[6]), 
+                cell_f(r[7]), 
+                cell_f(r[8])
+            ) + RESET + "│")
         elif (col_lite != -1):
-            formatted_row = "{}  {}  {}  |  {}  {}  {}  |  {}  {}  {}".format(
-                r[0] if col_lite != 0 else LITE + str(r[0]) + RESET,
-                r[1] if col_lite != 1 else LITE + str(r[1]) + RESET,
-                r[2] if col_lite != 2 else LITE + str(r[2]) + RESET,
-                r[3] if col_lite != 3 else LITE + str(r[3]) + RESET,
-                r[4] if col_lite != 4 else LITE + str(r[4]) + RESET,
-                r[5] if col_lite != 5 else LITE + str(r[5]) + RESET,
-                r[6] if col_lite != 6 else LITE + str(r[6]) + RESET,
-                r[7] if col_lite != 7 else LITE + str(r[7]) + RESET,
-                r[8] if col_lite != 8 else LITE + str(r[8]) + RESET,
+            formatted_row = "│ {}  {}  {}  |  {}  {}  {}  |  {}  {}  {} │".format(
+                cell_f(r[0]) if col_lite != 0 else LITE + str(cell_f(r[0])) + RESET,
+                cell_f(r[1]) if col_lite != 1 else LITE + str(cell_f(r[1])) + RESET,
+                cell_f(r[2]) if col_lite != 2 else LITE + str(cell_f(r[2])) + RESET,
+                cell_f(r[3]) if col_lite != 3 else LITE + str(cell_f(r[3])) + RESET,
+                cell_f(r[4]) if col_lite != 4 else LITE + str(cell_f(r[4])) + RESET,
+                cell_f(r[5]) if col_lite != 5 else LITE + str(cell_f(r[5])) + RESET,
+                cell_f(r[6]) if col_lite != 6 else LITE + str(cell_f(r[6])) + RESET,
+                cell_f(r[7]) if col_lite != 7 else LITE + str(cell_f(r[7])) + RESET,
+                cell_f(r[8]) if col_lite != 8 else LITE + str(cell_f(r[8])) + RESET,
             )
             print(formatted_row)
         else:
-            print("{}  {}  {}  |  {}  {}  {}  |  {}  {}  {}".format(r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[7], r[8]))
+            print("│ {}  {}  {}  │  {}  {}  {}  │  {}  {}  {} │".format(
+                cell_f(r[0]), 
+                cell_f(r[1]), 
+                cell_f(r[2]), 
+                cell_f(r[3]), 
+                cell_f(r[4]), 
+                cell_f(r[5]), 
+                cell_f(r[6]), 
+                cell_f(r[7]), 
+                cell_f(r[8])
+            ))
 
     def print(self):
-        self.print_row(0, False, 2)
-        self.print_row(1, True, 2)
-        self.print_row(2, False, 2)
+        print(TL)
+        self.print_row(0, False, -1)
+        self.print_row(1, False, -1)
+        self.print_row(2, False, -1)
         print(HL)
-        self.print_row(3, False, 2)
-        self.print_row(4, False, 2)
-        self.print_row(5, False, 2)
+        self.print_row(3, False, -1)
+        self.print_row(4, False, -1)
+        self.print_row(5, False, -1)
         print(HL)
-        self.print_row(6, False, 2)
-        self.print_row(7, False, 2)
-        self.print_row(8, False, 2)
+        self.print_row(6, False, -1)
+        self.print_row(7, False, -1)
+        self.print_row(8, False, -1)
+        print(BL)
+    
+    def print_cross_lite(self, x, y):
+        print(TL)
+        self.print_row(0, y == 0, x)
+        self.print_row(1, y == 1, x)
+        self.print_row(2, y == 2, x)
+        print(HL)
+        self.print_row(3, y == 3, x)
+        self.print_row(4, y == 4, x)
+        self.print_row(5, y == 5, x)
+        print(HL)
+        self.print_row(6, y == 6, x)
+        self.print_row(7, y == 7, x)
+        self.print_row(8, y == 8, x)
+        print(BL)
     
--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ from board import Board
 def main():
     test_file = "../test/s1.txt"
     board = Board.load_board(test_file)
-    board.print()
+    board.print_cross_lite(3,5)
+
 
 if __name__ == "__main__":
     main()
